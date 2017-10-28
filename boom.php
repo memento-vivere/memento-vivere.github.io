25ebd29a340b456bfba56abce283385dcf5e8c5d
memento-vivere <33181174+memento-vivere@users.noreply.github.com>
2017-10-28 18:49:40 +0300

    Create boom.php

---
+++ boom.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<style>
+body {
+    background-color: pink;
+}
+</style>
+<body>
+
+ 
+<title> SMILE</title>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR01oi2TRNqc-i_aZQEC00w-bPeqsoYER2FC6IEEO3zQl-znWOg" width="500" height="500">
+
+<?php
+echo "xo-xo-xo";
+?>
+
+</body>
+</html>
