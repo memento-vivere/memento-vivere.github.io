1cf6b4364194babd6326929eeb6f13361301867e
memento-vivere <33181174+memento-vivere@users.noreply.github.com>
2017-10-28 19:05:54 +0300

    Update boom.php

--- boom.php
+++ boom.php
@@ -6,10 +6,6 @@ body {
 </style>
 <body>
 
- 
-<title> SMILE</title>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR01oi2TRNqc-i_aZQEC00w-bPeqsoYER2FC6IEEO3zQl-znWOg" width="500" height="500">
-
 <?php
 echo "xo-xo-xo";
 ?>
